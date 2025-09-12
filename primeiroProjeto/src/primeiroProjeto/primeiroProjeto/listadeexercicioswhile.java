adf072f4fee3c01c4cf87f0193e77e2a23a46471
luiz-rb-0 <luiz.rgo.jr@gmail.com>
2025-09-12 14:50:47 -0300

    Add files via upload

---
+++ primeiroProjeto/src/primeiroProjeto/primeiroProjeto/listadeexercicioswhile.java
@@ -0,0 +1,21 @@
+package primeiroProjeto;
+
+public class listadeexercicioswhile {
+
+	public static void main(String[] args) {
+		
+		//1 - Contador:
+		
+		int contador = 10;
+		while(contador <= 30) {
+			System.out.println(contador);
+			contador++;
+		}
+
+		//2 - Soma de Números Digitados:
+		
+		
+		
+	}
+
+}
